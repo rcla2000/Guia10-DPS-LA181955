debbd7f4e9f13c74f37320dd2878fa2a75075392
Roberto Carlos López Abarca <rcla200031@gmail.com>
2022-11-01 18:37:24 -0600

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, SafeAreaView, StatusBar, LogBox } from "react-native";
+import base64 from "react-native-base64";
+import Auth from "./src/components/Auth";
+import firebase from "./src/utils/firebase";
+import "firebase/auth";
+import ListBirthday from "./src/components/ListBirthday";
+
+function btoa(data) {
+  return new base64(data, "binary").toString("base64");
+}
+
+function atob(data) {
+  return new base64(data, "base64").toString("binary");
+}
+
+LogBox.ignoreAllLogs(["Setting a timer"]);
 
 export default function App() {
+  const [user, setUser] = useState(undefined);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((response) => {
+      setUser(response);
+    });
+  }, []);
+
+  if (user === undefined) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={styles.background}>
+        {user ? <ListBirthday user={user} /> : <Auth />}
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  background: {
+    backgroundColor: "#15212b",
+    height: "100%",
   },
 });
---
+++ src/utils/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA6OgJAt_GPMR-mHugOgJYD9KWYbkw8G6o",
+  authDomain: "birthday-e42ca.firebaseapp.com",
+  projectId: "birthday-e42ca",
+  storageBucket: "birthday-e42ca.appspot.com",
+  messagingSenderId: "674983321260",
+  appId: "1:674983321260:web:4f0366ce0cd51b0c44c909"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export default firebase;
